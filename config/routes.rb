84a3384796ce602004cd8b103549634af14c58d0
Ellen Lee <leechinghui.tw@gmail.com>
2017-04-02 02:51:35 +0800

    update route for return and notify

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "products#index"
+
   resources :products, only: [:index, :show] do
     post :add_to_cart, on: :member
   end
+
   resource :cart
-  resources :orders
+  resources :orders do
+    post :checkout, on: :member
+  end
+
+
+  post 'pay2go/return'
+  post 'pay2go/notify'
 
   namespace :admin do
     root 'orders#index'
