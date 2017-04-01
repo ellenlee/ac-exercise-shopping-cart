0e69f51f7837780688c68985ceafa102e5e17bf4
Ellen Lee <leechinghui.tw@gmail.com>
2017-04-01 18:02:49 +0800

    admin can filter orders by different status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,14 +2,18 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_order, only: [:show, :edit, :update]
 
-
   def index
-    @orders = Order.all
+    if params[:order_status].present?
+      @orders = Order.where( order_status: params[:order_status])
+    elsif params[:payment_status].present?
+      @orders = Order.where( payment_status: params[:payment_status])
+    else
+      @orders = Order.all
+    end
   end
 
   def update
     @order.update(order_params)
-
     redirect_to admin_orders_path, notice: 'Date update.'
   end
 
