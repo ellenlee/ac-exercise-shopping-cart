f92f7e34b937a9526047b7b446434b3172f1cc82
Ellen Lee <leechinghui.tw@gmail.com>
2017-04-03 19:53:37 +0800

    Add Cart#add_line_item spec

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Cart, type: :model do
+
+  before do
+    @cart = Cart.create
+    @product = Product.create!( name: "product_1", price: 100)
+  end
+
+  describe "#add_line_item" do
+    it "it should add new product to line_items" do
+      new_line_item = @cart.add_line_item(@product)
+      new_line_item.save
+
+      expect(@cart.line_items.count).to eq(1)
+
+      line_item = @cart.line_items.last
+      expect(line_item.product).to eq(@product)
+      expect(line_item.quantity).to eq(1)
+    end
+
+    it "it should add new product to line_items" do
+      new_line_item1 = @cart.add_line_item(@product)
+      new_line_item1.save
+
+      new_line_item2 = @cart.add_line_item(@product)
+      new_line_item2.save
+
+      expect(@cart.line_items.count).to eq(1)
+
+      line_item = @cart.line_items.last
+      expect(line_item.product).to eq(@product)
+      expect(line_item.quantity).to eq(2)
+    end
+  end
+end
