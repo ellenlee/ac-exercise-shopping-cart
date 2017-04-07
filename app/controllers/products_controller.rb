92e9a574095b59624658e7c50921c9b0f9319057
Ellen Lee <leechinghui.tw@gmail.com>
2017-04-07 11:38:02 +0800

    user can add product to cart with AJAX

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,8 +11,12 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
     current_cart.add_cart_item(@product)
+    @item = current_cart.find_item_by(@product)
 
-    redirect_to root_path
+    # redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
 
 end
